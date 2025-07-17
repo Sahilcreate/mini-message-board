2a25ab20e3458b5a6d8b4f5e57eab3f72b95fc9c
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2025-07-17 14:17:13 +0530

    refactor(app): update route imports and middleware usage

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("node:path");
+
 const app = express();
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
 const messageRouter = require("./routes/messageRouter");
+const deleteRouter = require("./routes/deleteRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
 app.use("/message", messageRouter);
+app.use("/delete", deleteRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
